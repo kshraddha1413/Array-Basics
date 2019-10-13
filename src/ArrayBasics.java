bc7b57e9ee64f7570dd3b1861509173cac1a4e61
Travis Lewis <travis.of.lewis@gmail.com>
2019-10-12 22:12:03 -0400

    first commit

---
+++ src/ArrayBasics.java
@@ -0,0 +1,11 @@
+
+public class ArrayBasics {
+
+	public static void main(String[] args) {
+		int[] intarr= {10,20,30};//initialising and declaring array
+		for(int i=0;i<intarr.length;i++) {
+			System.out.println(intarr[i]);// Printing array element
+		}
+	}
+
+}
